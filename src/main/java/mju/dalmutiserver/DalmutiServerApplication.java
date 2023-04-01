6ddf48db5e42086b089bc0c3007a8a721d08c421
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-04-02 02:31:17 +0900

    initial setting

---
+++ src/main/java/mju/dalmutiserver/DalmutiServerApplication.java
@@ -0,0 +1,13 @@
+package mju.dalmutiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalmutiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalmutiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mju/dalmutiserver/DalmutiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mju.dalmutiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalmutiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
